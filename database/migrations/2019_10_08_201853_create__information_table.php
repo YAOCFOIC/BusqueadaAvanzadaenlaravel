2e4db6228c0e6651874c2ed628d9c1de39a4c6c5
John Doe <yesitalejandroosorio@gmail.com>
2019-10-08 17:32:00 -0500

    Primera parte

---
+++ database/migrations/2019_10_08_201853_create__information_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateInformationTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('Informations', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('telefono');
+            $table->string('Nit');
+            $table->string('nombre');
+            $table->string('Email');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('Informations');
+    }
+}
